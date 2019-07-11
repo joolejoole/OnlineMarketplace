5e7762771187c652efdd17b1e133bfaff832fea5
nakhuang <n.a.k.huang@gmail.com>
2019-07-11 16:12:11 -0400

    Init onion structure with unit of work and repository design patterns

---
+++ JoJo.Repo/Interface/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoJo.Repo
+{
+    public partial interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity GetById(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+        bool Insert(TEntity entity);
+
+        bool Remove(TEntity entity);
+
+        //int Edit(TEntity entity);
+
+        bool SaveChanges();
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ JoJo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JoJo.MvcApplication" Language="C#" %>
