426065027aa7e53c6759e5fbb0f3d74565bdc9d2
Quven Wang <45927147+quven213@users.noreply.github.com>
2019-07-12 09:15:30 -0400

    Filter Layout

---
+++ JoJo/Controllers/FilterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoJo.Controllers
+{
+    public class FilterController : Controller
+    {
+        // GET: Filter
+        public ActionResult Filter()
+        {
+            return View();
+        }
+    }
+}
