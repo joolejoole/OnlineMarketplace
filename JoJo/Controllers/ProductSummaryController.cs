4fd21bf4f4ad20f3c1196fd31af01863b3b04300
AlexYixiaoZhao <19940210+AlexYixiaoZhao@users.noreply.github.com>
2019-07-16 18:27:35 -0400

    Add Product Summary Page

---
+++ JoJo/Controllers/ProductSummaryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoJo.Controllers
+{
+    public class ProductSummaryController : Controller
+    {
+        // GET: ProductSummary
+        public ActionResult ShowProductSummary()
+        {
+            Models.Product SummaryData = new Models.Product(4);
+            //string ManuName = SummaryData.ManufactureIdName;
+            //ViewBag.ManufactureName = ManuName;
+            SummaryData.ToString();
+            return View(SummaryData);
+        }
+    }
+}
