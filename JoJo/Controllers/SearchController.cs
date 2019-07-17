b349f6214d2062c9c9ee9564b0c34a5a32105503
Realsosa <muraligunti.gunti@gmail.com>
2019-07-17 16:48:47 -0400

    fixed search bugs

--- JoJo/Controllers/SearchController.cs
+++ JoJo/Controllers/SearchController.cs
@@ -11,10 +11,12 @@ namespace JoJo.Controllers
 {
     public class SearchController : Controller
     {
-        JoJoEntities one = new JoJoEntities();
+        private static int gID;
+        //JoJoEntities one = new JoJoEntities();
         Sample s;
         public ActionResult Search()
         {
+            JoJoEntities one = new JoJoEntities();
             CatSubModel two = new CatSubModel();
             List<ProductCategory> prodCat = one.ProductCategory.ToList();
             ViewBag.prodCat = new SelectList(prodCat, "CategoryId", "CategoryName");
@@ -23,16 +25,23 @@ namespace JoJo.Controllers
 
         public JsonResult GetSubCat(int CategoryId)
         {
-
+            JoJoEntities one = new JoJoEntities();
             s = new Sample(one);
             one.Configuration.ProxyCreationEnabled = false;
-            return Json(s.subCategory(CategoryId), JsonRequestBehavior.AllowGet);
+            List<ProductSubCategory> subCat = one.ProductSubCategory.Where(x => x.CategoryId == CategoryId).ToList();
+            gID = CategoryId;
+            //s.subCategory(CategoryId)
+            return Json(subCat, JsonRequestBehavior.AllowGet);
+            //return Json(s.subCategory(CategoryId), JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
         public JsonResult getemp(string ename)
         {
-            var emp = (from x in one.ProductSubCategory where x.SubCategoryName.StartsWith(ename) select new { label = x.SubCategoryName }).ToList();
+            JoJoEntities one = new JoJoEntities();
+            var g = gID;
+            //List<ProductSubCategory> subCat = one.ProductSubCategory.Where(x => x.CategoryId == CategoryId).ToList();
+            var emp = (from x in one.ProductSubCategory where x.SubCategoryName.StartsWith(ename) && x.CategoryId==g select new { label = x.SubCategoryName }).ToList();
             s = new Sample(one);
             return Json(emp);
         }
